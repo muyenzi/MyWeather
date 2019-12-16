b72aa0b66c7fd5733a30717f7683d7f76792d351
Muyenzi Raissa <muyenziraissa@gmail.com>
2019-12-16 13:21:19 +0200

    last changes

--- app/src/main/java/com/moringaschool/myweather/drawer/RateStars.java
+++ app/src/main/java/com/moringaschool/myweather/drawer/RateStars.java
@@ -1,6 +1,10 @@
 package com.moringaschool.myweather.drawer;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -8,9 +12,21 @@ import com.moringaschool.myweather.R;
 
 public class RateStars extends AppCompatActivity {
 
+    private RatingBar ratingBar ;
+    private Button Submit_button;
+    private TextView RateTextView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_rate);
+        super.onCreate( savedInstanceState );
+        setContentView( R.layout.activity_rate );
+        ratingBar = findViewById(R.id.ratingBar);
+        Submit_button = findViewById(R.id.Submit_button);
+        RateTextView = findViewById(R.id.RateTextView);
+        Submit_button.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                RateTextView.setText( "your rating is; " + ratingBar.getRating() );
+            }
+        } );
     }
 }
